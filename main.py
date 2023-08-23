eb03822a2144a8170e009dd0b2055fa7ff379f48
Назокат Юлдашева <@corp.itstep>
2023-08-23 22:18:41 +0500

    selenium home work

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from selenium import webdriverimport time
-import os
-url = "https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0"
-a = requests.get(url)text = BeautifulSoup(a.text, 'html.parser')
-a = text.body.p.textprint(a)
-df = pd.DataFrame({'Name': [a]})
-df.to_excel('teams.xlsx')
-a = webdriver.Chrome()a.get("https://www.google.com")
-time.sleep(5)
-os.system(r'c:\Users\IsroilB\PycharmProjects\httpproject\teams.xlsx')
+from selenium import webdriver
+
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+
+# Setup chrome driver
+driver = webdriver.Chrome()
+
+# Navigate to the url
+driver.get('https://dribbble.com/tags/financial%20forecast')
+
+heading1 = driver.find_element(By.TAG_NAME, 'h1')
+print(heading1.text)
+# Find element by Tag Name
+# my_div = driver.find_element(By.TAG_NAME, 'div')
+# print(my_div.get_attribute("outerHTML"))
+
+# Close the driver
+driver.quit()
+
+
+
+
+
